7f831a7010bdb8f9350edac7af7718de92ffdfd7
sophephela <101864741+sophephela@users.noreply.github.com>
2023-09-03 15:29:25 +0200

    Add files via upload

---
+++ Save-Data-To-Cloud-Application/Save-Data-To-Cloud-Application-master/Register.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace Save_Data_To_Cloud
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fnameT = new System.Windows.Forms.TextBox();
+            this.lnameT = new System.Windows.Forms.TextBox();
+            this.emailT = new System.Windows.Forms.TextBox();
+            this.addressRtxb = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.saRd = new System.Windows.Forms.RadioButton();
+            this.nonRd = new System.Windows.Forms.RadioButton();
+            this.resetB = new System.Windows.Forms.Button();
+            this.saveB = new System.Windows.Forms.Button();
+            this.genderCmb = new System.Windows.Forms.ComboBox();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.ageS = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ageS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FirstName:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LastName:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Physical Address:";
+            // 
+            // fnameT
+            // 
+            this.fnameT.Location = new System.Drawing.Point(116, 168);
+            this.fnameT.Name = "fnameT";
+            this.fnameT.Size = new System.Drawing.Size(138, 21);
+            this.fnameT.TabIndex = 0;
+            // 
+            // lnameT
+            // 
+            this.lnameT.Location = new System.Drawing.Point(116, 204);
+            this.lnameT.Name = "lnameT";
+            this.lnameT.Size = new System.Drawing.Size(138, 21);
+            this.lnameT.TabIndex = 1;
+            // 
+            // emailT
+            // 
+            this.emailT.Location = new System.Drawing.Point(116, 236);
+            this.emailT.Name = "emailT";
+            this.emailT.Size = new System.Drawing.Size(138, 21);
+            this.emailT.TabIndex = 2;
+            // 
+            // addressRtxb
+            // 
+            this.addressRtxb.Location = new System.Drawing.Point(116, 300);
+            this.addressRtxb.Name = "addressRtxb";
+            this.addressRtxb.Size = new System.Drawing.Size(138, 96);
+            this.addressRtxb.TabIndex = 3;
+            this.addressRtxb.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(295, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Gender:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Date of birth:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(295, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Age:";
+            // 
+            // saRd
+            // 
+            this.saRd.AutoSize = true;
+            this.saRd.Location = new System.Drawing.Point(298, 335);
+            this.saRd.Name = "saRd";
+            this.saRd.Size = new System.Drawing.Size(42, 19);
+            this.saRd.TabIndex = 7;
+            this.saRd.TabStop = true;
+            this.saRd.Text = "SA";
+            this.saRd.UseVisualStyleBackColor = true;
+            // 
+            // nonRd
+            // 
+            this.nonRd.AutoSize = true;
+            this.nonRd.Location = new System.Drawing.Point(444, 335);
+            this.nonRd.Name = "nonRd";
+            this.nonRd.Size = new System.Drawing.Size(73, 19);
+            this.nonRd.TabIndex = 8;
+            this.nonRd.TabStop = true;
+            this.nonRd.Text = "Non-SA";
+            this.nonRd.UseVisualStyleBackColor = true;
+            // 
+            // resetB
+            // 
+            this.resetB.Location = new System.Drawing.Point(323, 408);
+            this.resetB.Name = "resetB";
+            this.resetB.Size = new System.Drawing.Size(75, 23);
+            this.resetB.TabIndex = 9;
+            this.resetB.Text = "Reset";
+            this.resetB.UseVisualStyleBackColor = true;
+            this.resetB.Click += new System.EventHandler(this.resetB_Click);
+            // 
+            // saveB
+            // 
+            this.saveB.Location = new System.Drawing.Point(444, 407);
+            this.saveB.Name = "saveB";
+            this.saveB.Size = new System.Drawing.Size(75, 23);
+            this.saveB.TabIndex = 10;
+            this.saveB.Text = "Save";
+            this.saveB.UseVisualStyleBackColor = true;
+            this.saveB.Click += new System.EventHandler(this.saveB_Click);
+            // 
+            // genderCmb
+            // 
+            this.genderCmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.genderCmb.FormattingEnabled = true;
+            this.genderCmb.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.genderCmb.Location = new System.Drawing.Point(389, 161);
+            this.genderCmb.Name = "genderCmb";
+            this.genderCmb.Size = new System.Drawing.Size(160, 23);
+            this.genderCmb.TabIndex = 4;
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(389, 202);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(160, 21);
+            this.datePicker.TabIndex = 5;
+            // 
+            // ageS
+            // 
+            this.ageS.Location = new System.Drawing.Point(389, 248);
+            this.ageS.Name = "ageS";
+            this.ageS.Size = new System.Drawing.Size(160, 21);
+            this.ageS.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(182, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(571, 446);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ageS);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.genderCmb);
+            this.Controls.Add(this.saveB);
+            this.Controls.Add(this.resetB);
+            this.Controls.Add(this.nonRd);
+            this.Controls.Add(this.saRd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.addressRtxb);
+            this.Controls.Add(this.emailT);
+            this.Controls.Add(this.lnameT);
+            this.Controls.Add(this.fnameT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)(this.ageS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fnameT;
+        private System.Windows.Forms.TextBox lnameT;
+        private System.Windows.Forms.TextBox emailT;
+        private System.Windows.Forms.RichTextBox addressRtxb;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton saRd;
+        private System.Windows.Forms.RadioButton nonRd;
+        private System.Windows.Forms.Button resetB;
+        private System.Windows.Forms.Button saveB;
+        private System.Windows.Forms.ComboBox genderCmb;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.NumericUpDown ageS;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Save-Data-To-Cloud-Application/Save-Data-To-Cloud-Application-master/Register.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Save_Data_To_Cloud
+{
+    public partial class Register : Form
+    {
+        SqlConnection con = new SqlConnection("Server=tcp:testing-innocent.database.windows.net,1433;Initial Catalog=Save-To-Cloud;Persist Security Info=False;User ID=Innocent;Password=************;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void resetB_Click(object sender, EventArgs e)
+        {
+            resetX();
+        }
+
+        private void resetX()
+        {
+            fnameT.Clear();
+            lnameT.Clear();
+            emailT.Clear();
+            addressRtxb.Clear();
+            genderCmb.SelectedIndex = -1;
+            ageS.Value = 0;
+            saRd.Checked = false;
+            nonRd.Checked = false;
+
+            fnameT.Focus();
+
+        }
+
+        private void saveB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string Citizen;
+                if (saRd.Checked)
+
+                    Citizen = "SA";
+                else
+                    Citizen = "Non SA";
+                SqlCommand com = new SqlCommand(""INSERT INTO Member VALUES('"+fnameT.Text+"','"+lnameT.Text+"','"+emailT.Text+"','"+addressRtxb.Text+"','"+genderCmb.SelectedItem.ToString()+"','"+datePicker.Value+"','"+ageS.Value+"','"+ Citizen+"')"", con);
+                try
+                {
+                    com.ExecuteNonQuery();
+                    resetX();
+
+                }
+                catch (Exception exx)
+                {
+
+                    MessageBox.Show("Failed to save to the database\n\n" + exx.Message);
+                }
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Failed to connect to the database\n\n" + ex.Message);
+            }
+
+        }
+    }
+}
